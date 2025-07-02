47fccbc6667d726d3d11d6b36ef76899a36094c1
Calihn1 <ehilacondob@unsa.edu.pe>
2025-07-02 08:21:00 -0500

    Decimo cuarto commit: Se agrego un nuevo componente user, que mostrara los usuarios

---
+++ proyectoAngular/my-dream-app/src/app/user/user.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  imports: [],
+  templateUrl: './user.html',
+  styleUrl: './user.css'
+})
+export class User implements OnInit {
+  @Input() nameUser;
+}
