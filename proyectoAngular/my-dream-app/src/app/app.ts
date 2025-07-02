26b39be4d1682046cc2bac14084c83d6411a20ff
Calihn1 <ehilacondob@unsa.edu.pe>
2025-07-01 23:11:55 -0500

    Tercer commit: Se implemento los datos de titulo, nombre, correo, pagina web en el archivo .ts del app

--- proyectoAngular/my-dream-app/src/app/app.ts
+++ proyectoAngular/my-dream-app/src/app/app.ts
@@ -7,6 +7,12 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
   protected title = 'my-dream-app';
+  name = 'Emanuel Hilacondo B.';
+  email = 'ehilacondob@unsa.edu.pe';
+  webpage = 'http://www.unsa.edu.pe';
 }
+
+
