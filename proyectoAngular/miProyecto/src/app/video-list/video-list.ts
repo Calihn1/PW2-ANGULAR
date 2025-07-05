1258dd9b2ba7bfdbaeed7820bf701bfd7c01e7ad
Calihn1 <ehilacondob@unsa.edu.pe>
2025-07-05 10:41:40 -0500

    Trigesimo commit: Se implemento las rutas de los videos y su titulo ademas del pipe y lo volvemos standalone

--- proyectoAngular/miProyecto/src/app/video-list/video-list.ts
+++ proyectoAngular/miProyecto/src/app/video-list/video-list.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SafeUrlPipe } from '../shared/pipes/safe-pipe'; // Importamos nuestro pipe
 
 @Component({
   selector: 'app-video-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, SafeUrlPipe], 
   templateUrl: './video-list.html',
-  styleUrl: './video-list.css'
+  styleUrls: ['./video-list.css']
 })
 export class VideoList {
+  title = "Últimos Videos de Marvel";
 
+  videos = [
+    {
+      title: 'Wakanda Forever (2022) Marvel Tráiler Oficial Español',
+      embedUrl: 'https://www.youtube.com/embed/R-A2p_G_3gQ'
+    },
+    {
+      title: "Marvel Studios' Thor: Love and Thunder | Official Trailer",
+      embedUrl: 'https://www.youtube.com/embed/Go8nTmfrQd8'
+    },
+    {
+      title: "Avengers: Infinity War - Tráiler Oficial",
+      embedUrl: 'https://www.youtube.com/embed/6ZfuNTqbHE8'
+    }
+  ];
 }
