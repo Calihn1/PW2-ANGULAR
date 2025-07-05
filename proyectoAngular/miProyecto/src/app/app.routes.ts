1af12f78f680fad86176352f4043bcd921881ca1
Calihn1 <ehilacondob@unsa.edu.pe>
2025-07-05 10:54:36 -0500

    Trigesimo segundo commit: Se implemento la ruta para los videos

--- proyectoAngular/miProyecto/src/app/app.routes.ts
+++ proyectoAngular/miProyecto/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { VideoList } from './video-list/video-list';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  // Ruta por defecto redirige a los videos
+  { path: '', redirectTo: 'videos', pathMatch: 'full' }, 
+  { path: 'videos', component: VideoList },
+];
