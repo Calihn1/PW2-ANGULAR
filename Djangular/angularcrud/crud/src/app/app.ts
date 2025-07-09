08285ef44f16f286fb71bb92cefa920c5777f8fa
Calihn1 <ehilacondob@unsa.edu.pe>
2025-07-09 08:28:50 -0500

    Octavo commit: Se implemento la logica de app.ts mostrara todos los datos por consola y se creo un arreglo

--- Djangular/angularcrud/crud/src/app/app.ts
+++ Djangular/angularcrud/crud/src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Api } from './api';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,24 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected title = 'crud';
+  movies = [
+    { id: 1, title: 'peli1', year: 2021 },
+    { id: 2, title: 'peli2', year: 2022 }
+  ];
+
+  constructor(private api: Api) {
+    this.getMovies();
+  }
+
+  getMovies = () => {
+    this.api.getAllMovies().subscribe(
+      data => {
+        console.log(data);
+        // this.movies = data;  // Puedes descomentar esta línea para asignar los datos recibidos
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
 }
