d8dad81cd4e0e668be4334b32a02e92d8920806d
Calihn1 <ehilacondob@unsa.edu.pe>
2025-07-09 08:47:37 -0500

    Decimo commit: Se implemento el HttpClient en main.ts en vez de modules por la version de angular

--- Djangular/angularcrud/crud/src/app/app.ts
+++ Djangular/angularcrud/crud/src/app/app.ts
@@ -1,18 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { Api } from './api';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'crud';
   movies = [
-    { id: 1, title: 'peli1', year: 2021 },
-    { id: 2, title: 'peli2', year: 2022 }
+    { id: 1, title: 'peli1', desc: 'desc1', year: 2021 },
+    { id: 2, title: 'peli2', desc: 'desc2', year: 2022 }
   ];
 
   constructor(private api: Api) {
